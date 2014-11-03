29dd0f6729eacbc98328f1ed6c69ef728cb96343
Christian Lehner <lehner.chri@gmail.com>
2014-11-03 17:54:13 +0100

    added sentence for testing purposes

--- nlp.js
+++ nlp.js
@@ -2,7 +2,10 @@ var spawn = require('child_process').spawn;
 var NewickParser = require('./NewickParser');
 var newickParser = new NewickParser();
 
-var sentences = ["The horse raced past the barn fell.\n"];
+var sentences = [
+  "The horse raced past the barn fell.\n",
+  "The cotton clothing is usually made of grows in Mississippi.\n"
+];
 
 var child = spawn("java", ["-Xmx1048m", "-jar", "parsers/berkeley/BerkeleyParser-1.7.jar", "-gr", "parsers/berkeley/eng_sm6.gr"]);
 
